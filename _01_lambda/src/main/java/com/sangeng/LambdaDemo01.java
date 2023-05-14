f9d4f02ea346eb7e887ac26cda12e9fbebf7d45a
meng <s81000890@hnu.edu.cn>
2023-05-14 22:52:55 +0800

    1

---
+++ _01_lambda/src/main/java/com/sangeng/LambdaDemo01.java
@@ -0,0 +1,61 @@
+package com.sangeng;
+
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntPredicate;
+
+public class LambdaDemo01 {
+    /*public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("新的线程中run方法被执行了");
+            }
+        }).start();
+
+    }*/
+
+    public static void main(String[] args) {
+//        demo01
+        /*new Thread(() -> {
+                System.out.println("新的线程中run方法被执行了");
+        }).start();*/
+        /**
+         * demo02
+         */
+        /*int i = calculateNum(new IntBinaryOperator() {
+            @Override
+            public int applyAsInt(int left, int right) {
+                return left + right;
+            }
+        });*/
+       /* int i = calculateNum((int left, int right) ->{
+            return left+right;
+        });
+
+        System.out.println(i);*/
+
+//        demo3
+
+
+    }
+
+    public static void printNum(IntPredicate predicate){
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        for (int i : arr) {
+            if(predicate.test(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static int calculateNum(IntBinaryOperator operator){
+        int a = 10;
+        int b = 20;
+        return operator.applyAsInt(a, b);
+    }
+
+
+
+
+
+}
