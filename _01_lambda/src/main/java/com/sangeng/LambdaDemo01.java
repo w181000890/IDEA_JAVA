61e94d53130718a4c39497c7bd1c47e575ce32a2
wm <164078906@qq.com>
2023-05-17 19:55:12 +0800

    提交

--- _01_lambda/src/main/java/com/sangeng/LambdaDemo01.java
+++ _01_lambda/src/main/java/com/sangeng/LambdaDemo01.java
@@ -1,6 +1,9 @@
 package com.sangeng;
 
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.IntBinaryOperator;
+import java.util.function.IntConsumer;
 import java.util.function.IntPredicate;
 
 public class LambdaDemo01 {
@@ -16,6 +19,7 @@ public class LambdaDemo01 {
 
     public static void main(String[] args) {
 //        demo01
+
         /*new Thread(() -> {
                 System.out.println("新的线程中run方法被执行了");
         }).start();*/
@@ -34,7 +38,7 @@ public class LambdaDemo01 {
 
         System.out.println(i);*/
 
-    // demo3
+        // demo3
         /*printNum(new IntPredicate() {
             @Override
             public boolean test(int value) {
@@ -44,33 +48,42 @@ public class LambdaDemo01 {
 //        printNum((int value) ->{
 //            return value%2==0;
 //        });
-        printNum((int value2) ->{
-            return value2%2==0;
+/**
+ * demo04
+ */
+
+//        Integer integer = typeConver(s -> {
+//            return Integer.valueOf(s);
+//        });
+//        System.out.println(integer);
+//
+//    }
+//        demo5
+        foreachArr(value -> {
+            if (value % 2 == 0) {
+                System.out.println(value);
+            }
         });
 
-
     }
 
-    public static void printNum(IntPredicate predicate){
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        for (int i : arr) {
-            if(predicate.test(i)){
-                System.out.println(i);
-            }
-        }
+    public static <R> R typeConver(Function<String, R> function) {
+        String str = "1235";
+        R result = function.apply(str);
+        return result;
     }
 
 
-
-
-    public static int calculateNum(IntBinaryOperator operator){
+    public static int calculateNum(IntBinaryOperator operator) {
         int a = 10;
         int b = 20;
         return operator.applyAsInt(a, b);
     }
 
-
-
-
-
+    public static void foreachArr(IntConsumer consumer) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i : arr) {
+            consumer.accept(i);
+        }
+    }
 }
